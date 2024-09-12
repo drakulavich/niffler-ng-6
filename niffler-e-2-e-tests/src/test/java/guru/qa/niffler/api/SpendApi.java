9df0df8167748f4912b97c35f810d5d23d3e765e
Anton Yakutovich <drakulavich@gmail.com>
2024-09-12 08:39:54 +0400

    WIP: hw1

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApi.java
@@ -2,11 +2,18 @@ package guru.qa.niffler.api;
 
 import guru.qa.niffler.model.SpendJson;
 import retrofit2.Call;
-import retrofit2.http.Body;
-import retrofit2.http.POST;
+import retrofit2.http.*;
 
 public interface SpendApi {
 
   @POST("internal/spends/add")
   Call<SpendJson> addSpend(@Body SpendJson spend);
+
+  @PATCH("internal/spends/edit")
+  @GET("internal/spends/{id}")
+  @GET("internal/spends/all")
+  @DELETE("internal/spends/remove")
+  @POST("internal/categories/add")
+  @PATCH("internal/categories/update")
+  @GET("internal/categories/all")
 }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -2,17 +2,51 @@ package guru.qa.niffler.page;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage {
   private final SelenideElement usernameInput = $("input[name='username']");
   private final SelenideElement passwordInput = $("input[name='password']");
   private final SelenideElement submitButton = $("button[type='submit']");
+  private final SelenideElement registerButton = $(".form__register");
+  private final SelenideElement signInButton = $(".form_sign-in");
 
   public MainPage login(String username, String password) {
     usernameInput.setValue(username);
     passwordInput.setValue(password);
     submitButton.click();
     return new MainPage();
+  }
+
+  public LoginPage setUsername(String username) {
+    usernameInput.setValue(username);
+    return this;
+  }
+
+  public LoginPage setPassword(String password) {
+    passwordInput.setValue(password);
+    return this;
+  }
+
+  public LoginPage submit() {
+    submitButton.click();
+    return this;
+  }
+
+  public void inputsAreVisible() {
+    usernameInput.shouldBe(visible);
+    passwordInput.shouldBe(visible);
+    submitButton.shouldBe(visible);
+  }
+
+  public LoginPage clickSignInButton() {
+    signInButton.click();
+    return this;
+  }
+
+  public RegisterPage openRegisterPage() {
+    registerButton.click();
+    return new RegisterPage();
   }
 }

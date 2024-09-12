0136b584998beab8de113ceb8b6d3722d4c9e7d6
Anton Yakutovich <drakulavich@gmail.com>
2024-09-12 21:26:14 +0400

    Add missing methods to SpendApiClient

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/SpendApiClient.java
@@ -1,10 +1,18 @@
 package guru.qa.niffler.api;
 
 import guru.qa.niffler.config.Config;
+import guru.qa.niffler.model.CategoryJson;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
-import lombok.SneakyThrows;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.jackson.JacksonConverterFactory;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SpendApiClient {
 
@@ -15,10 +23,90 @@ public class SpendApiClient {
 
   private final SpendApi spendApi = retrofit.create(SpendApi.class);
 
-  @SneakyThrows
   public SpendJson createSpend(SpendJson spend) {
-    return spendApi.addSpend(spend)
-        .execute()
-        .body();
+    final Response<SpendJson> response;
+    try {
+      response = spendApi.addSpend(spend).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(201, response.code());
+    return response.body();
+  }
+
+  public SpendJson updateSpend(SpendJson spend) {
+    final Response<SpendJson> response;
+    try {
+      response = spendApi.editSpend(spend).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  public SpendJson getSpend(int id, String username) {
+    final Response<SpendJson> response;
+    try {
+      response = spendApi.getSpend(id, username).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  public void deleteSpend(String username, List<String> ids) {
+    final Response<SpendJson> response;
+    try {
+    response = spendApi.removeSpend(username, ids).execute();
+    } catch (IOException e) {
+    throw new AssertionError(e);
+    }
+    assertEquals(202, response.code());
+  }
+
+  public List<SpendJson> getAllSpends(String username, CurrencyValues cur, String from, String to) {
+    final Response<SpendJson> response;
+    try {
+        response = spendApi.getAllSpends(username, cur, from, to).execute();
+    } catch (IOException e) {
+        throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return Collections.singletonList(response.body());
+  }
+
+  public CategoryJson createCategory(CategoryJson category) {
+    final Response<CategoryJson> response;
+    try {
+      response = spendApi.addCategory(category).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  public CategoryJson updateCategory(CategoryJson category) {
+    final Response<CategoryJson> response;
+    try {
+    response = spendApi.updateCategory(category).execute();
+    } catch (IOException e) {
+    throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  public List<CategoryJson> getAllCategories(String username, boolean excludeArchived) {
+    final Response<CategoryJson> response;
+    try {
+    response = spendApi.getAllCategories(username, excludeArchived).execute();
+    } catch (IOException e) {
+    throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return Collections.singletonList(response.body());
   }
 }

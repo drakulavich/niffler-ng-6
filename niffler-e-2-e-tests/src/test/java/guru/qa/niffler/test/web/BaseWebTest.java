0d4b91fa3414ec72c0bcafa3dcd1105d5dfb38d7
Anton Yakutovich <drakulavich@gmail.com>
2024-09-17 20:00:11 +0400

    Rebased on develop

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/BaseWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/BaseWebTest.java
@@ -2,9 +2,10 @@ package guru.qa.niffler.test.web;
 
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.extension.BrowserExtension;
+import guru.qa.niffler.jupiter.extension.UsersQueueExtension;
 import org.junit.jupiter.api.extension.ExtendWith;
 
-@ExtendWith(BrowserExtension.class)
+@ExtendWith({BrowserExtension.class, UsersQueueExtension.class})
 public class BaseWebTest {
     protected static final Config CFG = Config.getInstance();
 }

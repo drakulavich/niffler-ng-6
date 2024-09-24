7695d3b44b9af19fbe65eb2547dd25949da34c8e
Anton Yakutovich <drakulavich@gmail.com>
2024-09-24 21:28:57 +0400

    Use SpendDbClient in CategoryExtension

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/SpendDbClient.java
@@ -6,6 +6,7 @@ import guru.qa.niffler.data.dao.impl.CategoryDaoJdbc;
 import guru.qa.niffler.data.dao.impl.SpendDaoJdbc;
 import guru.qa.niffler.data.entity.spend.CategoryEntity;
 import guru.qa.niffler.data.entity.spend.SpendEntity;
+import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
 
 public class SpendDbClient {
@@ -21,6 +22,19 @@ public class SpendDbClient {
     }
     return SpendJson.fromEntity(
         spendDao.create(spendEntity)
+    );
+  }
+
+  public CategoryJson createCategory(CategoryJson category) {
+    CategoryEntity categoryEntity = CategoryEntity.fromJson(category);
+    return CategoryJson.fromEntity(
+      categoryDao.create(categoryEntity)
+    );
+  }
+
+  public void deleteCategory(CategoryJson category) {
+    categoryDao.deleteCategory(
+      CategoryEntity.fromJson(category)
     );
   }
 }

50433989d29f08e955177613fa62706c8465a1d0
Anton Yakutovich <drakulavich@gmail.com>
2024-09-26 23:29:26 +0400

    Implement AuthUserDao / AuthAuthorityDao

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthUserDao.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/dao/AuthUserDao.java
@@ -1,11 +1,11 @@
 package guru.qa.niffler.data.dao;
 
-import guru.qa.niffler.data.entity.user.UserEntity;
+import guru.qa.niffler.data.entity.auth.UserEntity;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserdataUserDAO {
+public interface AuthUserDao {
   UserEntity createUser(UserEntity user);
   Optional<UserEntity> findById(UUID id);
   Optional<UserEntity> findByUsername(String username);

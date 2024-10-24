60d3813ed290a5f83a58de7018d041261430df89
Anton Yakutovich <drakulavich@gmail.com>
2024-10-24 21:03:38 +0400

    Add UsersApiClient that works with internal REST endpoints

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UsersApi.java
@@ -0,0 +1,47 @@
+package guru.qa.niffler.api;
+
+import guru.qa.niffler.model.UserJson;
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+import java.util.List;
+
+public interface UsersApi {
+
+  // users
+  @GET("internal/users/current")
+  Call<UserJson> currentUser(@Query("username") String username);
+
+  @GET("internal/users/all")
+  Call<List<UserJson>> allUsers(@Query("username") String username,
+                                @Query("searchQuery") String searchQuery);
+
+  @POST("internal/users/update")
+  Call<UserJson> updateUserInfo(@Body UserJson user);
+
+  // invitations
+  @POST("internal/friends/send")
+  Call<UserJson> sendInvitation(@Query("username") String username,
+                                @Query("targetUsername") String targetUsername);
+
+  @POST("internal/friends/accept")
+  Call<UserJson> acceptInvitation(@Query("username") String username,
+                                  @Query("targetUsername") String targetUsername);
+
+  @POST("internal/friends/decline")
+  Call<UserJson> declineInvitation(@Query("username") String username,
+                                   @Query("targetUsername") String targetUsername);
+
+  // friends
+  @GET("internal/friends/all")
+  Call<List<UserJson>> friends(@Query("username") String username,
+                               @Query("searchQuery") String searchQuery);
+
+  @DELETE("internal/friends/remove")
+  Call<Void> removeFriend(@Query("username") String username,
+                          @Query("targetUsername") String targetUsername);
+}

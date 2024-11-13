579681c3fccda4bcb836eb72e9091b21a804b176
Anton Yakutovich <drakulavich@gmail.com>
2024-11-13 20:31:18 +0400

    Sync up with stream 'Allure lifecycle'

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingWebTest.java
@@ -1,12 +1,21 @@
 package guru.qa.niffler.test.web;
 
 import com.codeborne.selenide.Selenide;
+import guru.qa.niffler.jupiter.annotation.ScreenShotTest;
 import guru.qa.niffler.jupiter.annotation.Spending;
 import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.UserJson;
 import guru.qa.niffler.page.LoginPage;
 import guru.qa.niffler.page.MainPage;
+import guru.qa.niffler.utils.ScreenDiffResult;
 import org.junit.jupiter.api.Test;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import static com.codeborne.selenide.Selenide.$;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class SpendingWebTest extends BaseWebTest {
   private final MainPage mainPage = new MainPage();
@@ -45,5 +54,26 @@ public class SpendingWebTest extends BaseWebTest {
         .getHeader().addSpendingPage().createSpend("Museum", "Art", 300)
         .checkAlert("New spending is successfully created")
         .getSpendingTable().checkTableContains("Museum");
+  }
+
+  @User(
+    spendings = @Spending(
+      category = "Обучение",
+      description = "Обучение Advanced 2.0",
+      amount = 79990
+    )
+  )
+  @ScreenShotTest("img/expected-stat.png")
+  void checkStatComponentTest(UserJson user, BufferedImage expected) throws IOException {
+    Selenide.open(CFG.frontUrl(), LoginPage.class)
+      .login(user.username(), user.testData().password());
+
+    // sleep to prevent failed test
+    Selenide.sleep(2000);
+    BufferedImage actual = ImageIO.read($("canvas[role='img']").screenshot());
+    assertFalse(new ScreenDiffResult(
+      actual,
+      expected
+    ));
   }
 }

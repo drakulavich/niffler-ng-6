8d0d03cd5a81db337d3c7f7e8fac5d2ca38396fe
Anton Yakutovich <drakulavich@gmail.com>
2024-11-19 21:31:58 +0400

    First version

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/StatConditions.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/condition/StatConditions.java
@@ -36,45 +36,51 @@ public class StatConditions {
   }
 
   @Nonnull
-  public static WebElementsCondition color(@Nonnull Color... expectedColors) {
+  public static WebElementsCondition statBubbles(@Nonnull Bubble... expectedBubbles) {
     return new WebElementsCondition() {
 
-      private final String expectedRgba = Arrays.stream(expectedColors).map(c -> c.rgb).toList().toString();
+      private final String expectedValues = Arrays.stream(expectedBubbles)
+        .map(b -> "Color: " + b.color().rgb + ", Text: " + b.text())
+        .toList()
+        .toString();
 
       @NotNull
       @Override
       public CheckResult check(Driver driver, List<WebElement> elements) {
-        if (ArrayUtils.isEmpty(expectedColors)) {
-          throw new IllegalArgumentException("No expected colors given");
+        if (ArrayUtils.isEmpty(expectedBubbles)) {
+          throw new IllegalArgumentException("No expected bubbles given");
         }
-        if (expectedColors.length != elements.size()) {
-          final String message = String.format("List size mismatch (expected: %s, actual: %s)", expectedColors.length, elements.size());
+        if (expectedBubbles.length != elements.size()) {
+          final String message = String.format("List size mismatch (expected: %s, actual: %s)", expectedBubbles.length, elements.size());
           return rejected(message, elements);
         }
+
         boolean passed = true;
-        final List<String> actualRgbaList = new ArrayList<>();
+        final List<String> actualBubbles = new ArrayList<>();
         for (int i = 0; i < elements.size(); i++) {
           final WebElement elementToCheck = elements.get(i);
-          final Color colorToCheck = expectedColors[i];
+          final Color colorToCheck = expectedBubbles[i].color();
+          final String textToCheck = expectedBubbles[i].text();
           final String rgba = elementToCheck.getCssValue("background-color");
-          actualRgbaList.add(rgba);
+          final String text = elementToCheck.getText();
+          actualBubbles.add("Color: " + rgba + ", Text: " + text);
           if (passed) {
-            passed = colorToCheck.rgb.equals(rgba);
+            passed = colorToCheck.rgb.equals(rgba) && textToCheck.equals(text);
           }
         }
         if (!passed) {
-          final String actualRgba = actualRgbaList.toString();
+          final String actualValues = actualBubbles.toString();
           final String message = String.format(
-            "List colors mismatch (expected: %s, actual: %s)", expectedRgba, actualRgba
+            "List bubbles mismatch (expected: %s, actual: %s)", expectedValues, actualValues
           );
-          return rejected(message, actualRgba);
+          return rejected(message, actualValues);
         }
         return accepted();
       }
 
       @Override
       public String toString() {
-        return expectedRgba;
+        return expectedValues;
       }
     };
   }
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/StatComponent.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/component/StatComponent.java
@@ -2,7 +2,8 @@ package guru.qa.niffler.page.component;
 
 import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
-import guru.qa.niffler.condition.Color;
+import guru.qa.niffler.condition.Bubble;
+import guru.qa.niffler.condition.StatConditions;
 import guru.qa.niffler.utils.ScreenDiffResult;
 import io.qameta.allure.Step;
 
@@ -14,7 +15,6 @@ import java.io.IOException;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
-import static guru.qa.niffler.condition.StatConditions.color;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class StatComponent extends BaseComponent<StatComponent> {
@@ -47,8 +47,8 @@ public class StatComponent extends BaseComponent<StatComponent> {
 
   @Step("Check that stat bubbles contains colors {expectedColors}")
   @Nonnull
-  public StatComponent checkBubblesColors(Color... expectedColors) {
-    bubbles.should(color(expectedColors));
+  public StatComponent checkBubbles(Bubble... expectedBubbles) {
+    bubbles.should(StatConditions.statBubbles(expectedBubbles));
     return this;
   }
 }

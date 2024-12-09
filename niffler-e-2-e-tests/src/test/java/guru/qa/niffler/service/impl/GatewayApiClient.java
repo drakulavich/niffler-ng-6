bb1173b1d26a2bbe3179d9ac359e384d0e5c7804
Anton Yakutovich <drakulavich@gmail.com>
2024-12-09 16:56:32 +0400

    Add tests for public API
    Improve coverage to UserControllerTest

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/GatewayApiClient.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/service/impl/GatewayApiClient.java
@@ -2,6 +2,7 @@ package guru.qa.niffler.service.impl;
 
 import guru.qa.niffler.api.GatewayApi;
 import guru.qa.niffler.api.core.RestClient;
+import guru.qa.niffler.model.rest.FriendJson;
 import guru.qa.niffler.model.rest.UserJson;
 import io.qameta.allure.Step;
 import retrofit2.Response;
@@ -22,12 +23,76 @@ public class GatewayApiClient extends RestClient {
     this.gatewayApi = create(GatewayApi.class);
   }
 
-  @Step("send /api/friends/all GET request to niffler-gateway")
+  @Step("[Public API] send /api/friends/all GET request")
   public List<UserJson> allFriends(@Nonnull String bearerToken,
                                    @Nullable String searchQuery) {
     final Response<List<UserJson>> response;
     try {
       response = gatewayApi.allFriends(bearerToken, searchQuery).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  @Step("[Public API] send /api/users/all GET request")
+  public List<UserJson> allUsers(@Nonnull String bearerToken,
+                                 @Nullable String searchQuery) {
+    final Response<List<UserJson>> response;
+    try {
+      response = gatewayApi.allUsers(bearerToken, searchQuery).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  @Step("[Public API] send /api/friends/remove DELETE request")
+  public void removeFriend(@Nonnull String bearerToken,
+                           @Nonnull String friendId) {
+    final Response<Void> response;
+    try {
+      response = gatewayApi.removeFriend(bearerToken, friendId).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+  }
+
+  @Step("[Public API] send /api/friends/invite POST request")
+  public UserJson sendInvitation(@Nonnull String bearerToken,
+                                 @Nonnull FriendJson friend) {
+    final Response<UserJson> response;
+    try {
+      response = gatewayApi.sendInvitation(bearerToken, friend).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  @Step("[Public API] send /api/friends/accept POST request")
+  public UserJson acceptInvitation(@Nonnull String bearerToken,
+                                   @Nonnull FriendJson friend) {
+    final Response<UserJson> response;
+    try {
+      response = gatewayApi.acceptInvitation(bearerToken, friend).execute();
+    } catch (IOException e) {
+      throw new AssertionError(e);
+    }
+    assertEquals(200, response.code());
+    return response.body();
+  }
+
+  @Step("[Public API] send /api/friends/decline POST request")
+  public UserJson declineInvitation(@Nonnull String bearerToken,
+                                    @Nonnull FriendJson friend) {
+    final Response<UserJson> response;
+    try {
+      response = gatewayApi.declineInvitation(bearerToken, friend).execute();
     } catch (IOException e) {
       throw new AssertionError(e);
     }

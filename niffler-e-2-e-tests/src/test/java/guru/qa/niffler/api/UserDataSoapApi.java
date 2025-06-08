a514264d43a8a2efae8b38b1877c6f62d5fec2b9
Anton Yakutovich <drakulavich@gmail.com>
2025-06-08 13:05:07 +0400

    Use downloaded WSDL schema

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserDataSoapApi.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/api/UserDataSoapApi.java
@@ -1,14 +1,14 @@
 package guru.qa.niffler.api;
 
-import guru.qa.jaxb.userdata.AcceptInvitationRequest;
-import guru.qa.jaxb.userdata.AllUsersRequest;
-import guru.qa.jaxb.userdata.CurrentUserRequest;
-import guru.qa.jaxb.userdata.DeclineInvitationRequest;
-import guru.qa.jaxb.userdata.FriendsPageRequest;
-import guru.qa.jaxb.userdata.RemoveFriendRequest;
-import guru.qa.jaxb.userdata.SendInvitationRequest;
-import guru.qa.jaxb.userdata.UserResponse;
-import guru.qa.jaxb.userdata.UsersResponse;
+import guru.qa.niffler.userdata.wsdl.AcceptInvitationRequest;
+import guru.qa.niffler.userdata.wsdl.AllUsersRequest;
+import guru.qa.niffler.userdata.wsdl.CurrentUserRequest;
+import guru.qa.niffler.userdata.wsdl.DeclineInvitationRequest;
+import guru.qa.niffler.userdata.wsdl.FriendsPageRequest;
+import guru.qa.niffler.userdata.wsdl.RemoveFriendRequest;
+import guru.qa.niffler.userdata.wsdl.SendInvitationRequest;
+import guru.qa.niffler.userdata.wsdl.UserResponse;
+import guru.qa.niffler.userdata.wsdl.UsersResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.Headers;
